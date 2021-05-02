1be5dcdc8820d147e20184cd2dd2016e99242252
karim <sarieddinekarim63@gmail.com>
2021-05-02 20:19:49 +0300

    'm'

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -51,7 +51,7 @@ export const Navbar = () => {
         <Nav>
             <ul>
                 <li><Link to='/'>Home</Link></li>
-                <li><Link to='#/contact'>Contact us</Link></li>
+                <li><Link to='/contact'>Contact us</Link></li>
             </ul>
         </Nav>
     )
